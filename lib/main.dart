a93a857747e1ea64a9c8ae42c1f5bc8fab312015
emrebingor <emrebingor5@gmail.com>
2025-08-04 21:24:18 +0300

    Crashlytics integration

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:node_labs_movie_app/data/provider/selected_movie_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:node_labs_movie_app/core/bloc_providers.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:node_labs_movie_app/utils/service/crashlytics/crashlytics_initialize.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  CrashlyticsInitialize.init();
   runApp(const MyApp());
 }
 
---
+++ lib/utils/service/crashlytics/crashlytics_initialize.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class CrashlyticsInitialize {
+  static void init() {
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+  }
+}
